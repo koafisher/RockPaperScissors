63e5b79d1cbf477ebea2f18bf52c327d8780e119
koafisher <koafisher@comcast.net>
2017-11-02 23:27:35 +0000

    setup?

--- RPS/routes/index.js
+++ RPS/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/rps',function(req, res, next) {
+  console.log("Playing RPS");
+  res.render('index', { title: 'RPS' });
+});
+
 module.exports = router;
